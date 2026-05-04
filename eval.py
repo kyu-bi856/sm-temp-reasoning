0d9ae4a3e393168ae16e895ef5534f0721eac83d
kyu-bi856 <quinnbernardin@gmail.com>
2026-05-04 17:03:58 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -71,7 +71,7 @@ def main():
 
   for i in tqdm(range(len(dataset))): 
     out = prompt_on_TLA(model, tokenizer, dataset[i]) 
-    if extract_answer(out) == dataset[i]["gold_answer"]
+    if extract_answer(out) == dataset[i]["gold_answer"]:
 
   print(f"\n Accuracy = {correct / len(dataset)}")
   
