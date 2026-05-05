45b75ffcbe8b5ffb23f2a42f372368c7133a05d7
kyu-bi856 <quinnbernardin@gmail.com>
2026-05-05 12:48:37 -0400

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -30,7 +30,7 @@ def main():
   correct_counters = {"dataset_name" : Counter(), "task" : Counter(), "level" : Counter(), "metric_type" : Counter()}
   
 
-  with open(args.input_dir, "r", encoding="utf-8") as file:
+  with open(f"responses/{args.input_dir}.json", "r", encoding="utf-8") as file:
     dataset = json.load(file)
   
   for point in tqdm(dataset): 
