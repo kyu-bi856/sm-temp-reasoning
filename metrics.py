4f582c46d14a5f7b3236b1c53b9f007322d2cda7
kyu-bi856 <quinnbernardin@gmail.com>
2026-05-05 12:55:13 -0400

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -57,6 +57,7 @@ def main():
   for key in dataset_info: 
     for counter_key in dataset_info[key]: 
       acc = correct_counters[key][counter_key] / dataset_info[key][counter_key]
+      print(acc)
       accuracy_computations[f"{key}/{counter_key}"] = acc
 
   print("Computations Complete") 
