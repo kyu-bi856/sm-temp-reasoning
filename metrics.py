e295d4f0a01a1d1bc6d2f4d4bc38de56f91ef0fe
kyu-bi856 <quinnbernardin@gmail.com>
2026-05-05 12:42:10 -0400

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -61,7 +61,7 @@ def main():
 
   print("Computations Complete") 
 
-  output_path = os.path.join(args.output_dir, f"{args.input_dir}_metrics.json")
+  output_path = os.path.join(args.result_dir, f"{args.input_dir}_metrics.json")
 
   print(f"Output path for metrics is: {output_path}")
   
