fa81b5c10c30cba3c7ec4600d02661035c3033d5
kyu-bi856 <quinnbernardin@gmail.com>
2026-05-06 12:34:51 -0400

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -50,7 +50,7 @@ def set_model(model_path):
       per_device_train_batch_size = 2,
         gradient_accumulation_steps = 4,
         warmup_steps = 5,
-        max_steps = 150,
+        max_steps = 500,
        # num_train_epochs = 1, # Set this instead of max_steps for full training runs
         learning_rate = 2e-4,
         logging_steps = 1,
