854142bb6ebbcc7918e77ca1f89e0b96db378d1d
kyu-bi856 <quinnbernardin@gmail.com>
2026-05-07 14:43:39 -0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -51,7 +51,7 @@ def get_args():
   parser = argparse.ArgumentParser()
 
   parser.add_argument("--model_path", type=str, required=True, default=None)  # models for evaluation
-  parser.add_argument("--dataset_path", type=str, required=True, default=None)
+  parser.add_argument("--dataset_path", type=str, required=False, default="TensorTemplar/TIME-Lite-Atomic")
 
   return parser.parse_args()
   
